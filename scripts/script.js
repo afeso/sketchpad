3627db5f921c4e20ab3164435748f6b34a849037
Franklyn Afeso <lilflexy4real@gmail.com>
2017-08-07 07:10:22 +0100

    Initial completion

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	
+	//change the .box class to boxed
+	$('.box').addClass('boxed');
+	
+	
+	// reset the grid when the button is clicked
+	$('button').on('click', function() {
+		$('.box').css("background-color", "white");
+		
+		//prompt user for a pixel size when the reset button is clicked
+		// and make it the value of the variable gridSize
+		gridSize = parseInt(prompt("Please Specify the size of Grids you want. Default is 16"));
+		  
+		
+		//change the size of the grid according to the value of the gridSize variable
+		
+		if (gridSize > 16) {
+			$('.box').css({'height' :+ gridSize + 'px',
+				   'width' : + gridSize + 'px',
+				  'float' : 'left',
+				  'display' : 'block'});
+		} else if (gridSize < 16) {
+			alert("grid size cannot be less than 16. Please choose a higher size. Will default tolast used size.");
+		} else {
+			$('.box').css({'height' : '16px',
+				   'width' : '16px',
+				  'float' : 'left',
+				  'clear' : 'right'});
+		}
+	});
+	
+	
+	// this function changes the color of the divs to a random one when the mouse hovers over it
+	$('.box').hover(function() {
+		
+		var r = Math.floor(Math.random() * 256);
+		var g = Math.floor(Math.random() * 256);
+		var b = Math.floor(Math.random() * 256);
+		
+		//var i = 1;
+		
+		$(this).css("background-color", "rgb(" +r +"," + g + "," + b +")");
+				
+	/*	while ($(this).mouseover() = true {
+			i++;
+			
+			if (i >= 11) {
+				$(this).css("background-color" , "black");
+			} else {
+				$(this).css("background-color", "rgb(" +r +"," + g + "," + b +")");
+			}
+		} */
+	
+	});
+	
+	
+});
